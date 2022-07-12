cb80c1663f8b86b759d488c309e9df3c8a62d5e4
adelndf <you@example.com>
2022-07-12 23:45:10 +0300

    removing multer3

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(
-      "mongodb+srv://iadelndf:2260507a@blog-cluster.dtvp1cj.mongodb.net/?retryWrites=true&w=majority"
-    );
+    const connect = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB connected: ${connect.connection.host}`);
   } catch (error) {
